a9857463dba04afdee4facf56e7f68dd760af63b
李丹 <906356050@qq.com>
2020-08-27 16:31:37 +0800

    2d

--- search_a_2D_matrix_2.py
+++ search_a_2D_matrix_2.py
@@ -11,7 +11,7 @@ def searchMatrix(matrix, target):
             matrix.pop(last)
         //1.consider the last row pop out  then matrix is empty
         //2.consider every column del then matrix[[],[],[],[]]
-        // above all ,consider two different situation of edgh condition
+        // above all ,consider two different  edge condition
         if matrix:
             last = len(matrix)-1
             if last>=0:
@@ -21,7 +21,7 @@ def searchMatrix(matrix, target):
                      return False
         else:
             return False
-    if target ==left_corner:
+    if target == left_corner:
         return True
 
 
